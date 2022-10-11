23a23040a0bede01dc72d10db20fb387eb2f633a
Asif Mahmud Shimon <ams.eee09@gmail.com>
2022-10-11 10:14:33 +0600

    column select widget update

--- lib/esm/mui-datatable-select-columns.component.d.ts
+++ lib/esm/mui-datatable-select-columns.component.d.ts
@@ -1,2 +1,10 @@
-/// <reference types="react" />
-export default function MuiDatatableSelectColumns(): JSX.Element;
+import { ReactNode } from 'react';
+import { MuiDatatableColumnOptions } from './mui-datatable.reducer';
+export declare type MuiDatatableSelectColumnsProps = {
+    showIcon?: boolean;
+    icon?: ReactNode;
+    label?: ReactNode;
+    renderValue?: (selected: string[], choices: string[]) => ReactNode;
+    renderListItem?: (column: MuiDatatableColumnOptions, index: number) => ReactNode;
+};
+export default function MuiDatatableSelectColumns({ showIcon, icon, label, renderValue, renderListItem, }: MuiDatatableSelectColumnsProps): JSX.Element;
--- src/mui-datatable-select-columns.component.tsx
+++ src/mui-datatable-select-columns.component.tsx
@@ -1,16 +1,56 @@
-import React from 'react';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
-import useMuiDatatable from './use-mui-datatable.hook';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import Checkbox from '@mui/material/Checkbox';
 import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Typography from '@mui/material/Typography';
+import React, { ReactNode } from 'react';
+import { MuiDatatableColumnOptions } from './mui-datatable.reducer';
+import useMuiDatatable from './use-mui-datatable.hook';
 
-export default function MuiDatatableSelectColumns() {
+export type MuiDatatableSelectColumnsProps = {
+  showIcon?: boolean;
+  icon?: ReactNode;
+  label?: ReactNode;
+  renderValue?: (selected: string[], choices: string[]) => ReactNode;
+  renderListItem?: (
+    column: MuiDatatableColumnOptions,
+    index: number
+  ) => ReactNode;
+};
+
+function defaultRenderValue(selected: string[], choices: string[]) {
+  return selected.length === 0
+    ? ''
+    : selected.length === 1
+    ? 'Showing 1 column'
+    : selected.length === choices.length
+    ? 'Showing all columns'
+    : `Showing ${selected.length} columns`;
+}
+
+function defaultRenderListItem(
+  column: MuiDatatableColumnOptions,
+  index: number
+) {
+  return (
+    <>
+      {column.renderHeader && column.renderHeader}
+      {!column.renderHeader && (column.header || `Column #${index}`)}
+    </>
+  );
+}
+
+export default function MuiDatatableSelectColumns({
+  showIcon = true,
+  icon = <VisibilityOutlinedIcon sx={{ mr: 1 }} />,
+  label = <Typography variant='body1'>Columns</Typography>,
+  renderValue = defaultRenderValue,
+  renderListItem = defaultRenderListItem,
+}: MuiDatatableSelectColumnsProps) {
   const {
     columnVisibilityChoices,
     visibleColumns,
@@ -23,14 +63,17 @@ export default function MuiDatatableSelectColumns() {
     columnVisibilityChoices.includes(col.property)
   );
 
+  // field's label component
+  const selectLabel = (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      {showIcon && icon}
+      {label}
+    </Box>
+  );
+
   return (
     <FormControl sx={{ minWidth: '12rem' }}>
-      <InputLabel id='mui-datatable-select-columns'>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <VisibilityOutlinedIcon sx={{ mr: 1 }} />
-          <Typography variant='body1'>Columns</Typography>
-        </Box>
-      </InputLabel>
+      <InputLabel id='mui-datatable-select-columns'>{selectLabel}</InputLabel>
       <Select
         labelId='mui-datatable-select-columns'
         multiple
@@ -38,29 +81,13 @@ export default function MuiDatatableSelectColumns() {
         onChange={event => {
           setVisibleColumns(event.target.value as string[]);
         }}
-        label={
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <VisibilityOutlinedIcon sx={{ mr: 1 }} />
-            <Typography variant='body1'>Columns</Typography>
-          </Box>
-        }
-        renderValue={selected => {
-          return selected.length === 0
-            ? ''
-            : selected.length === 1
-            ? 'Showing 1 column'
-            : selected.length === columnVisibilityChoices.length
-            ? 'Showing all columns'
-            : `Showing ${selected.length} columns`;
-        }}
+        label={selectLabel}
+        renderValue={selected => renderValue(selected, columnVisibilityChoices)}
       >
         {columns.map((col, index) => (
           <MenuItem key={index} value={col.property}>
             <Checkbox checked={visibleColumns.includes(col.property)} />
-            <ListItemText>
-              {col.renderHeader && col.renderHeader}
-              {!col.renderHeader && (col.header || `Column #${index}`)}
-            </ListItemText>
+            <ListItemText>{renderListItem(col, index)}</ListItemText>
           </MenuItem>
         ))}
       </Select>
